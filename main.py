b273af044f16e217c80461999022f0c82b088dfc
JK 184429 <s184429@student.pg.edu.pl>
2025-01-05 23:48:43 +0100

    implemented multiprocessing

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import random
 import csv
+import string
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import Manager
+
 import faker
 import datetime
 import chardet
@@ -101,11 +105,17 @@ def save_to_csv(file_name, data):
         writer.writerows(data)
 
 
-def gen_uniq_id(fake_gen, ids: list):
+def gen_id(length):
+    characters = string.digits + string.ascii_uppercase
+
+    return ''.join(random.choices(characters, k=length))
+
+
+def gen_uniq_id(ids: list):
     u_id = None
 
     while u_id is None or u_id in ids:
-        u_id = fake_gen.bothify('######')
+        u_id = gen_id(9)
 
     ids.append(u_id)
     return u_id
@@ -122,21 +132,21 @@ def check_for_date_conflict(start_date, end_date, gear_id, g_dict) -> bool:
     return False
 
 
-def gen_clients(fake_gen, uids: list, ids: list, n: int) -> [Client]:
+def gen_clients(uids: list, idx: int, ids: list, n: int) -> [Client]:
+    fake = faker.Faker("pl_PL")
     client_list = []
 
-    for _ in range(n):
-        uid = gen_uniq_id(fake_gen, uids)
-        ids.append(uid)
-        client_list.append(Client(uid, fake_gen.first_name(), fake_gen.last_name(),
-                                  fake_gen.bothify('###-###-###')))
+    for i in range(n):
+        ids.append(uids[idx + i])
+        client_list.append(Client(uids[idx + i], fake.first_name(), fake.last_name(), fake.bothify('###-###-###')))
     return client_list
 
 
-def gen_ski_centers(fake_gen, ids: list, n: int) -> [SkiCenter]:
+def gen_ski_centers(ids: list, n: int) -> [SkiCenter]:
+    fake = faker.Faker("pl_PL")
     centers = []
     for _ in range(n):
-        centers.append(SkiCenter(gen_uniq_id(fake_gen, ids), fake_gen.company()))
+        centers.append(SkiCenter(gen_uniq_id(ids), fake.company()))
     return centers
 
 
@@ -149,18 +159,19 @@ def gen_random_idx(idx_list: [int], count: int) -> int:
         return random.randint(0, count)
 
 
-def gen_gear(fake_gen, uids: list, n: int, centers: [SkiCenter], ids: list):
+def gen_gear(uids: list, idx: int, centers: [SkiCenter], ids: list, n: int):
+    fake = faker.Faker("pl_PL")
     gear_list, gear_ex_list = [], []
     center_count = len(centers)
     center_ids = list(range(center_count))
-    for _ in range(n):
+
+    for i in range(n):
         ski_idx = gen_random_idx(center_ids, center_count - 1)
-        gear_id = gen_uniq_id(fake_gen, uids)
-        ids.append(gear_id)
+        ids.append(uids[idx + i])
         gear_type = rand_g_type()
 
-        gear_list.append(Gear(gear_id, centers[ski_idx].id, gear_type))
-        gear_ex_list.append(GearExcel(gear_id, centers[ski_idx].id, gear_type, rand_prod(), fake_gen.vin(),
+        gear_list.append(Gear(uids[idx + i], centers[ski_idx].id, gear_type))
+        gear_ex_list.append(GearExcel(uids[idx + i], centers[ski_idx].id, gear_type, rand_prod(), fake.vin(),
                                       rand_size(gear_type)))
     return gear_list, gear_ex_list
 
@@ -176,11 +187,10 @@ def save_service(service_list: [Service], suffix: str):
                  for service in service_list])
 
 
-def gen_leased_gear(fake_gen, ids: list, n: int, g_list: list, c_list: list,
-                    start_date, end_date, g_dict, p_dict, suffix: str):
+def gen_leased_gear(ids: list, idx: int, n: int, g_list: list, c_list: list,
+                    start_date, end_date, p_dict, g_dict, n_dict):
     lease_list = []
-    client_count, gear_count = len(c_list), len(g_list)
-    client_ids, gear_ids = list(range(client_count)), list(range(gear_count))
+    fake = faker.Faker("pl_PL")
     for a in range(n):
         g_id = None
         conflict = True
@@ -188,55 +198,59 @@ def gen_leased_gear(fake_gen, ids: list, n: int, g_list: list, c_list: list,
 
         if a % 10000 == 0:
             print("another 10000, currently: " + str(a))
-        if len(lease_list) == 100000:
-            save_leased_gear(lease_list, suffix)
-            lease_list = []
 
         while conflict:
-            g_id = g_list[gen_random_idx(gear_ids, gear_count - 1)]
-            lease_date = fake_gen.date_between(start_date=start_date, end_date=end_date)
+            g_id = random.choice(g_list)
+            lease_date = fake.date_between(start_date=start_date, end_date=end_date)
             planned_date = lease_date + datetime.timedelta(days=random.randint(0, 4))
             ret_date = planned_date + datetime.timedelta(days=random.randint(0, 3))
             conflict = check_for_date_conflict(lease_date, ret_date, g_id, g_dict)
+
             if not conflict:
                 conflict = check_for_date_conflict(lease_date, ret_date, g_id, p_dict)
+                if not conflict:
+                    conflict = check_for_date_conflict(lease_date, ret_date, g_id, n_dict)
+
         if g_id in g_dict:
             g_dict[g_id].append([lease_date, ret_date])
         else:
             g_dict[g_id] = [[lease_date, ret_date]]
-        lease_list.append(LeasedGear(gen_uniq_id(fake_gen, ids), g_id, random.choice(c_list), lease_date,
-                                     planned_date, ret_date))
-    save_leased_gear(lease_list, suffix)
+
+        lease_list.append(LeasedGear(ids[idx + a], g_id, random.choice(c_list), lease_date,  planned_date, ret_date))
+
+    return lease_list
 
 
-def gen_services(fake_gen, ids: list, n: int, g_list: list, start_date, end_date, g_dict, p_dict, suffix: str):
+def gen_services(ids: list, idx: int, n: int, g_list: list, start_date, end_date, p_dict, g_dict, n_dict):
     service_list = []
+    fake = faker.Faker("pl_PL")
     for a in range(n):
         g_id = None
         conflict = True
         service_date, planned_date, return_date = None, None, None
 
         if a % 10000 == 0:
-            print("another 1000, currently: " + str(a))
-        if len(service_list) == 100000:
-            save_leased_gear(service_list, suffix)
-            service_list = []
+            print("another 10000, currently: " + str(a))
 
         while conflict:
-            g_id = g_list[random.randint(0, len(g_list) - 1)]
-            service_date = fake_gen.date_between(start_date=start_date, end_date=end_date)
+            g_id = random.choice(g_list)
+            service_date = fake.date_between(start_date=start_date, end_date=end_date)
             planned_date = service_date + datetime.timedelta(days=random.randint(0, 7))
             return_date = planned_date + datetime.timedelta(days=random.randint(0, 3))
             conflict = check_for_date_conflict(service_date, return_date, g_id, g_dict)
             if not conflict:
                 conflict = check_for_date_conflict(service_date, return_date, g_id, p_dict)
+                if not conflict:
+                    conflict = check_for_date_conflict(service_date, return_date, g_id, n_dict)
+
         if g_id in g_dict:
             g_dict[g_id].append([service_date, return_date])
         else:
             g_dict[g_id] = [[service_date, return_date]]
-        service_list.append(Service(gen_uniq_id(fake_gen, ids), g_id, service_date, planned_date,
-                                    return_date))
-    save_service(service_list, suffix)
+
+        service_list.append(Service(ids[idx + a], g_id, service_date, planned_date, return_date))
+
+    return service_list
 
 
 def export_services(service_list: [Service], suffix: str):
@@ -282,39 +296,75 @@ def get_unique_sample(src: list, result: list, ids: list, sample_size: int):
     return chosen_samples
 
 
-def gen_dimensions():
-    g_ids, c_ids, ski_ids = [], [], []
-    # ^ lists of used ids for gear, clients, leases, ski centers, services ^
-    gc_t1, gg_t1, gc_t2, gg_t2 = [], [], [], []
+def gen_id_set(ids: list, count: int):
+    for _ in range(count):
+        gen_uniq_id(ids)
 
-    #   Set T1
-    clients = gen_clients(fake, c_ids, gc_t1, 50000)
-    print("clients t1 generated")
-    clients_t2 = gen_clients(fake, c_ids, gc_t2, 2000)
-    print("clients t2 generated")
-    old_clients = get_unique_sample(clients, clients_t2, gc_t2, 3000)
-    print("clients t1 moved to clients t2")
+
+def modify_and_add_clients(new_clients, old_clients):
+    fake = faker.Faker("pl_PL")
     for c in old_clients:
         c.last_name = fake.last_name()
-    export_clients(clients, 'T1')
-    export_clients(clients_t2, 'T2')
 
-    ski_centers = gen_ski_centers(fake, ski_ids, 1000)
+    return new_clients + old_clients
+
+
+def add_old_gear(new_gear, old_gear, new_gear_excel, old_gear_excel, ng_ids, og_ids, count):
+    for i in range(count):
+        if og_ids[i] in ng_ids:
+            new_gear_excel.append(old_gear_excel[i])
+            new_gear.append(old_gear[i])
+
+    return new_gear, new_gear_excel
+
+
+def gen_dimensions(c_count1, c_count2, g_count1, g_count2, center_count, c_sample_size, g_sample_size):
+    gc_t1, gg_t1, gc_t2, gg_t2, ski_ids = [], [], [], [], []
+    c_count2_diff = c_count2 - c_sample_size
+    g_count2_diff = g_count2 - g_sample_size
+    ski_centers = gen_ski_centers(ski_ids, center_count)
     print("centers t1 generated")
+    with Manager() as manager:
+        g_ids, c_ids = manager.list(), manager.list()
+        mc_t1, mg_t1, mc_t2, mg_t2 = manager.list(), manager.list(), manager.list(), manager.list()
+        with ProcessPoolExecutor() as executor:
+            p1 = executor.submit(gen_id_set, g_ids, g_count1 + g_count2)
+            p2 = executor.submit(gen_id_set, c_ids, c_count1 + c_count2)
 
-    gear, gear_excel = gen_gear(fake, g_ids, 100000, ski_centers, gg_t1)
-    print("gear t1 generated")
-    gear_t2, gear_excel_t2 = gen_gear(fake, g_ids, 5000, ski_centers, gg_t2)
-    print("gear t2 generated")
-    get_unique_sample(gear, gear_t2, gg_t2, 5000)
-    print("gear t1 moved to gear t2")
-    for i in range(4998, len(gear)):
-        if gear[i] in gear_t2:
-            gear_excel_t2.append(gear_excel[i])
-    print("gear excel t1 moved to gear excel t2")
+            p1.result()
+            p2.result()
+            print("Dimension ids generated")
+
+            p1 = executor.submit(gen_clients, c_ids, 0, mc_t1, c_count1)
+            p2 = executor.submit(gen_clients, c_ids, c_count1, mc_t2, c_count2_diff)
+            p3 = executor.submit(gen_gear, g_ids, 0, ski_centers, mg_t1, g_count1)
+            p4 = executor.submit(gen_gear, g_ids, g_count1, ski_centers, mg_t2, g_count2_diff)
+
+            clients_t1 = p1.result()
+            clients_t2 = p2.result()
+            gear_t1, gear_excel_t1 = p3.result()
+            gear_t2, gear_excel_t2 = p4.result()
+            print("Clients and gear generated for T1 and T2")
+            p1 = executor.submit(get_unique_sample, clients_t1, clients_t2, mc_t2, c_sample_size)
+            p2 = executor.submit(get_unique_sample, gear_t1, gear_t2, mg_t2, g_sample_size)
+
+            old_clients = p1.result()
+            p2.result()
+
+            p1 = executor.submit(add_old_gear, gear_t2, gear_t1, gear_excel_t2, gear_excel_t1, mg_t2, mg_t1, g_count1)
+            p2 = executor.submit(modify_and_add_clients, clients_t2, old_clients)
+            clients_t2 = p2.result()
+            gear_t2, gear_excel_t2 = p1.result()
+
+            gc_t1, gg_t1, gc_t2, gg_t2, = list(mc_t1), list(mg_t1), list(mc_t2), list(mg_t2)
+            print("Some clients and gear moved from T1 to T2")
+            print("Gear excel t1 moved to gear excel t2")
+
+    export_clients(clients_t1, 'T1')
+    export_clients(clients_t2, 'T2')
     export_centers(ski_centers, 'T1')
     export_centers(ski_centers, 'T2')
-    export_gear(gear, gear_excel, 'T1')
+    export_gear(gear_t1, gear_excel_t1, 'T1')
     export_gear(gear_t2, gear_excel_t2, 'T2')
     print("dimension exports done")
     return gc_t1, gg_t1, gc_t2, gg_t2
@@ -331,40 +381,76 @@ def gen_time_periods(start_date, count, span):
     return periods
 
 
+def parallel_facts_gen(l_ids, s_ids, start_idx, c_per_p, gear, clients, period, dicts, idx_list, suffix):
+    with ProcessPoolExecutor() as executor:
+        l_results = list(
+            executor.map(lambda x: gen_leased_gear(l_ids, c_per_p * x + start_idx, c_per_p, gear, clients,
+                                                   period[x][0], period[x][1], dicts[x], dicts[x + 1],
+                                                   dicts[x + 2]
+                                                   ),
+                         idx_list
+                         ))
+
+        s_results = list(
+            executor.map(lambda x: gen_services(s_ids, c_per_p * x + start_idx, c_per_p, gear,
+                                                period[x][0], period[x][1], dicts[x], dicts[x + 1],
+                                                dicts[x + 2]
+                                                ),
+                         idx_list
+                         ))
+
+        save_leased_gear(l_results, suffix)
+        save_service(s_results, suffix)
+
+
+def gen_facts(period1: list, period2: list, c_per_p1: int, c_per_p2: int,
+              gear_t1, gear_t2, clients_t1, clients_t2):
+
+    period1_count = len(period1)
+    period2_count = len(period2)
+    fact_count1 = c_per_p1 * period1_count
+    fact_count2 = c_per_p2 * period2_count
+    even_idx = [i for i in range(period1_count) if i % 2 == 0]
+    odd_idx = [i for i in range(period1_count) if i % 2 != 0]
+    even_idx2 = [i for i in range(period2_count) if i % 2 == 0]
+    odd_idx2 = [i for i in range(period2_count) if i % 2 != 0]
+
+    with Manager() as manager:
+        dicts = [manager.dict() for _ in range(period1_count + 2)]
+        l_ids, s_ids = manager.list(), manager.list()
+
+        with ProcessPoolExecutor() as executor:
+            future1 = executor.submit(gen_id_set, l_ids, fact_count1 + fact_count2)
+            future2 = executor.submit(gen_id_set, s_ids, fact_count1 + fact_count2)
+
+            future1.result()
+            future2.result()
+            print("Fact id generated")
+
+        parallel_facts_gen(l_ids, s_ids, 0, c_per_p1, gear_t1, clients_t1, period1, dicts, even_idx, 'T1')
+        print("T1 around half finished")
+        parallel_facts_gen(l_ids, s_ids, 0, c_per_p1, gear_t1, clients_t1, period1, dicts, odd_idx, 'T1')
+        print("T1 finished")
+
+        parallel_facts_gen(l_ids, s_ids, fact_count1, c_per_p2, gear_t2, clients_t2, period2, dicts, even_idx2, 'T2')
+        print("T2 around half finished")
+        parallel_facts_gen(l_ids, s_ids, fact_count1, c_per_p2, gear_t2, clients_t2, period2, dicts, odd_idx2, 'T2')
+        print("T2 finished")
+
+
 if __name__ == '__main__':
-    fake = faker.Faker("pl_PL")
     export_leased_gear([], 'T1')
     export_services([], 'T1')
     export_leased_gear([], 'T2')
     export_services([], 'T2')
-    t1_start_date = datetime.datetime(2023, 1, 1)
-    t1_end_date = datetime.datetime(2023, 12, 31)
-    t2_start_date = datetime.datetime(2024, 1, 1)
-    t2_end_date = datetime.datetime(2024, 12, 31)
-    lg_ids, serv_ids = [], []
 
-    c_t1, g_t1, c_t2, g_t2 = gen_dimensions()
-    coll_dict = {g: [] for g in g_t1}
-    prev_coll = {}
+    time_periods = gen_time_periods(datetime.datetime(2023, 1, 1), 9, 30)
+    init_date2 = time_periods[9][1] + datetime.timedelta(days=1)
+    time_periods2 = gen_time_periods(init_date2, 1, 30)
 
-    time_periods = gen_time_periods(t1_start_date, 9, 30)
+    c_t1, g_t1, c_t2, g_t2 = gen_dimensions(50000, 5000, 100000, 10000, 1000, 2000, 5000)
 
-    for period in time_periods:
-        gen_leased_gear(fake, lg_ids, 100000, g_t1, c_t1, period[0], period[1], coll_dict, prev_coll, 'T1')
-        gen_services(fake, serv_ids, 100000, g_t1, period[0], period[1], coll_dict, prev_coll, 'T1')
-        print("one loop finished")
-        prev_coll = coll_dict
-        coll_dict = {}
-
-    print("T1 finished")
-    time_periods = gen_time_periods(time_periods[9][1] + datetime.timedelta(days=1), 1, 30)
-
-    for period in time_periods:
-        gen_leased_gear(fake, lg_ids, 50000, g_t1, c_t1, period[0], period[1], coll_dict, prev_coll, 'T2')
-        gen_services(fake, serv_ids, 50000, g_t1, period[0], period[1], coll_dict, prev_coll, 'T2')
-        print("one loop finished")
-        prev_coll = coll_dict
-        coll_dict = {}
+    gen_facts(time_periods, time_periods2, 100000, 10000, g_t1, g_t2, c_t1, c_t2)
 
 with open('clientsT1.csv', 'rb') as f:
     print(chardet.detect(f.read()))
